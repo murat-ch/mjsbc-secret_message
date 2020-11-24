1ae0d3345e7d32943f3c0b4ba41d69468de4efe8
murat-ch <muratik.ch@gmail.com>
2020-11-24 12:50:48 -0500

    1-done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hi there');
+const { hash } = window.location;
+const message = atob(hash.replace('#', ''));
+if(message) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+    document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+    const messageInput = document.querySelector('#message-input');
+    const linkInput = document.querySelector('#link-input');
+    const messageForm = document.querySelector('#message-form');
+    const linkForm = document.querySelector('#link-form');
+    const encrypted = btoa(messageInput.value);
+    linkInput.value = `${window.location}#${encrypted}`;
+    messageForm.classList.add('hide');
+    linkForm.classList.remove('hide');
+    linkInput.select();
+
+
+
+
+});
+
